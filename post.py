56686a45946762e1565e5e54a6a250db829a3c98
tentenlee <tentenlee100@gmail.com>
2019-01-26 03:08:06 +0800

    [Refactor] 修改showroom貼文改成class

--- post.py
+++ post.py
@@ -4,57 +4,66 @@ from PTTLibrary import PTT
 import datetime
 from config import *
 
-KickOtherLogin = False
 
-ID = PTT_ACCOUNT
-Password = PTT_PASSWORD
+class Showroom(object):
 
-PTTBot = PTT.Library(kickOtherLogin=False)
+    @staticmethod
+    def get_content() -> str:
+        contents = ""
+        contents += "SR個別成員直播      https://www.showroom-live.com/campaign/akb48_sr" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "概要:" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "除了成員各自showroom配信外，還有一些活動番組的showroom" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "大量開設instagram也會有個人的直播" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "只要關於SNS的LIVE都可在這邊討論" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "showroom過濾網頁   https://tenten.tw/48showroom/" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "Instagram日飯製入口&分析站 http://ig48.gutas.net/" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "twitter日飯製入口&分析站 http://tw48.net/" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "AKE48 TeamTP instagram彙整網站 https://tenten.tw/tpeig/" + "\r\n"
+        contents += "" + "\r\n"
+        contents += "showroom 48 過濾 新工具: chrome extensions https://bit.ly/2HjQza9" + "\r\n"
+        contents += "" + "\r\n"
 
-ErrCode = PTTBot.login(ID, Password)
+        return contents
 
-if not PTTBot.isLoginSuccess():
-    PTTBot.Log('登入失敗')
-    sys.exit()
 
-# 這個範例是如何PO文
-# 第一個參數是你要PO文的板
-# 第二個參數是文章標題
-# 第三個參數是文章內文
-# 第四個參數是發文類別       1
-# 第五個參數是簽名檔        	0
-# 回傳值 就是錯誤碼
-for i in range(1):
+if __name__ == '__main__':
     st = datetime.datetime.fromtimestamp(time.time()).strftime('%y%m%d')
     title = '[LIVE] ' + st + ' Showroom+SNS直播 實況閒聊文'
-    contents = '''
-SR個別成員直播      https://www.showroom-live.com/campaign/akb48_sr \r\n
-\r\n
-概要: \r\n\r\n
-除了成員各自showroom配信外，還有一些活動番組的showroom \r\n\r\n
-大量開設instagram也會有個人的直播 \r\n\r\n
-只要關於SNS的LIVE都可在這邊討論\r\n
-\r\n
-\r\n
-showroom過濾網頁   https://tenten.tw/48showroom/\r\n
-SR日飯製入口&分析站 http://sr48.net/\r\n
-//停止使用中\r\n
-\r\n
-Instagram日飯製入口&分析站 http://ig48.gutas.net/\r\n
-\r\n
-twitter日飯製入口&分析站 http://tw48.net/\r\n
-\r\n
-TPE instagram彙整網站 https://tenten.tw/tpeig/ \r\n
-\r\n
-showroom 48 過濾 新工具: chrome extensions https://bit.ly/2HjQza9 \r\n
-\r\n
-\r\n
-'''
+    contents = Showroom.get_content()
 
-    ErrorCode = PTTBot.post('AKB48', title, contents, 0, 0)
+    print(contents)
+
+    ### 發文相關資訊填寫
+    ID = PTT_ACCOUNT
+    Password = PTT_PASSWORD
+    board = 'AKB48'
+    KickOtherLogin = False
+
+    ###
+    PTTBot = PTT.Library(kickOtherLogin=KickOtherLogin)
+
+    ErrCode = PTTBot.login(ID, Password)
+    if ErrCode != PTT.ErrorCode.Success:
+        PTTBot.Log('登入失敗')
+        sys.exit()
+
+    ErrorCode = PTTBot.post(board, title, contents, 0, 0)
     if ErrorCode == PTT.ErrorCode.Success:
-        PTTBot.Log('在 Test 板發文成功')
+        PTTBot.Log('在' + board + '板發文成功')
+        PTTBot.throwWaterBall('emperor', '今日閒聊文已發文')
+
     elif ErrorCode == PTT.ErrorCode.NoPermission:
         PTTBot.Log('發文權限不足')
     else:
         PTTBot.Log('在 Test 板發文失敗')
+
+    PTTBot.logout()
